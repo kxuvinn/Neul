0b13dd27dff41ce4a7c3a469b42277abe55f6d97
ehyunna <ehyunna@sookmyung.ac.kr>
2025-06-15 21:42:59 +0900

    Update IntroPanel.java

--- src/main/java/IntroPanel.java
+++ src/main/java/IntroPanel.java
@@ -12,16 +12,15 @@ public class IntroPanel extends JPanel {
         setLayout(new BorderLayout());
         setBackground(new Color(91, 89, 153)); // #5b5999
 
-        // 중앙 텍스트 + 버튼 패널
+     
         JPanel centerPanel = new JPanel();
         centerPanel.setBackground(new Color(91, 89, 153));
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         centerPanel.setBorder(BorderFactory.createEmptyBorder(150, 0, 0, 0));
 
-        // 부제목
-        // 원래 코드
+     
 
-// → 이걸 아래처럼 수정
+
         JLabel subtitle = new JLabel(
                 "<html><div style='text-align: center; width: 100%;'>늘 곁에 있는,<br>나만의 감정 수면 파트너</div></html>",
                 SwingConstants.CENTER
@@ -38,7 +37,7 @@ public class IntroPanel extends JPanel {
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 
-        // 버튼 두 개
+        // 로그인, 회원가입 버튼
         JButton loginBtn = new JButton("로그인");
         JButton signupBtn = new JButton("회원가입");
         customizeButton(loginBtn);
@@ -59,7 +58,7 @@ public class IntroPanel extends JPanel {
         centerPanel.add(subtitle);
         centerPanel.add(Box.createVerticalStrut(40));
         centerPanel.add(title);
-        centerPanel.add(Box.createVerticalStrut(70)); // NEUL 아래 50px 간격
+        centerPanel.add(Box.createVerticalStrut(70)); 
         centerPanel.add(buttonPanel);
         centerPanel.add(Box.createVerticalGlue());
 
@@ -75,13 +74,6 @@ public class IntroPanel extends JPanel {
         button.setFont(new Font("SansSerif", Font.PLAIN, 16));
     }
 
-    //public static void main(String[] args) {
-        //JFrame frame = new JFrame("Intro");
-        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setSize(1440, 1024);
-        //frame.setLocationRelativeTo(null);
-        //frame.add(new IntroPanel());
-        //frame.setVisible(true);
-    //}
+   
 }
-//test 용 main임 주석처리하고 사용하기
+
