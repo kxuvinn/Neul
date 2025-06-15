488d6faf02637769cf3ab7c108d93dca949d41fa
semijang <jsm2sem@gmail.com>
2025-06-15 21:28:22 +0900

    Update NEUL_MAIN.java

--- src/main/java/NEUL_MAIN.java
+++ src/main/java/NEUL_MAIN.java
@@ -6,19 +6,19 @@ public class NEUL_MAIN {
             SleepDataService dataService = new SleepDataService();
             RandomSleepInformation infoService = new RandomSleepInformation();
 
-            // dummy controller 먼저 생성
+            // dummy controller 생성
             ScreenController controller = new ScreenController(null);
 
-            // PanelManager 생성 시 controller는 일단 전달
+            // PanelManager 생성 시 controller 전달
             PanelManager manager = new PanelManager(controller, dataService, infoService);
 
             // controller 내부에 manager 연결
-            controller.setMainWindow(manager);  // setMainWindow() 메서드 필요
+            controller.setMainWindow(manager); 
 
-            // controller 다시 PanelManager에 주입 (선택 사항)
+            // controller를 PanelManager에
             manager.setController(controller);
 
-            // 이제 controller가 살아 있으니 패널 등록 진행
+            // 패널 등록
             manager.registerScreens(dataService, infoService);
 
             controller.navigateTo("intro");
