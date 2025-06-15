4b1d1d1e0afebd93c7a873ab723bfc39e7cd0b3b
semijang <jsm2sem@gmail.com>
2025-06-15 21:58:33 +0900

    Update NEUL_MAIN.java

--- src/main/java/NEUL_MAIN.java
+++ src/main/java/NEUL_MAIN.java
@@ -3,24 +3,28 @@ import javax.swing.SwingUtilities;
 public class NEUL_MAIN {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            // 데이터를 관리하는 서비스 객체 생성
             SleepDataService dataService = new SleepDataService();
+            
+            // 수면에 관한 정보를 랜덤으로 제공하는 서비스 객체 생성
             RandomSleepInformation infoService = new RandomSleepInformation();
 
-            // dummy controller 생성
+            // 화면 전환을 담당하는 controller 생성
             ScreenController controller = new ScreenController(null);
 
-            // PanelManager 생성 시 controller 전달
+            // PanelManager 생성 시 controller, dataService, infoService 전달
             PanelManager manager = new PanelManager(controller, dataService, infoService);
 
-            // controller 내부에 manager 연결
+            // controller에 main 화면으로 manager 연결
             controller.setMainWindow(manager); 
 
-            // controller를 PanelManager에
+            // manager에 controller 연결
             manager.setController(controller);
 
-            // 패널 등록
+            // manager에 패널 등록
             manager.registerScreens(dataService, infoService);
 
+            // intro 화면으로 전환
             controller.navigateTo("intro");
         });
     }
